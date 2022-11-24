dd4da6245d6ac40b0aeda9c5703914d3afbbdfdb
francm9 <francano368@gmail.com>
2022-11-24 10:01:58 +0100

    modify inicio

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -5,7 +5,24 @@ import { getUserInfo } from "../services/user.js"
 import {BsHouseDoorFill} from 'react-icons/bs'
 import { GiPowerButton } from 'react-icons/gi'
 import { Sidebar } from "./Sidebar"
+import ShowPlaylist from "./ShowPlaylist"
 import "../styles/Inicio.css"
+
+const Descripcion = () => {
+  return (
+    <div id="sntc-button">
+        <h1 className="sentence">
+          Crea una playlist a tu gusto y totalmente personalizada eligiendo
+          tus artistas y canciones favoritas
+        </h1>
+        <button className="create-btn">CREAR PLAYLIST A TU GUSTO</button>
+        <h1 className="sentence">
+          O bien obtén una creada por nosotros y descubre nueva música
+        </h1>
+        <button className="get-btn">OBTENER</button>
+      </div>
+  )
+}
 
 const Inicio = () => {
   let code = window.location.search.split("=")[1]
@@ -24,17 +41,7 @@ const Inicio = () => {
     <div>
       <Navbar btn1={<BsHouseDoorFill/>} btn2={<GiPowerButton/>} user={user.display_name}/>
       <Sidebar token={token} />
-      <div id="sntc-button">
-        <h1 className="sentence">
-          Crea una playlist a tu gusto y totalmente personalizada eligiendo
-          tus artistas y canciones favoritas
-        </h1>
-        <button className="create-btn">CREAR PLAYLIST A TU GUSTO</button>
-        <h1 className="sentence">
-          O bien obtén una creada por nosotros y descubre nueva música
-        </h1>
-        <button className="get-btn">OBTENER</button>
-      </div>
+      <ShowPlaylist />
     </div>
   );
 };
---
+++ src/components/ShowPlaylist.js
@@ -0,0 +1,7 @@
+const ShowPlaylist = () => {
+    return(
+        <h1>Soy la playlist</h1>
+    )
+}
+
+export default ShowPlaylist;
